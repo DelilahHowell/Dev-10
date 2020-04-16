f7bc58d36e6e3f34c8fd8fb406e273f90de4cc69
DelilahHowell <60522515+DelilahHowell@users.noreply.github.com>
2020-04-16 13:46:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var xhr = new XMLHttpRequest();                 // Create XMLHttpRequest object
+
+xhr.onload = function() {                       // When readystate changes
+  // The following conditional check will not work locally - only on a server
+  //if(xhr.status === 200) {                      // If server status was ok
+    responseObject = JSON.parse(xhr.responseText);
+
+    // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
+    var newContent = '';
+    for (var i = 0; i < responseObject.events.length; i++) { // Loop through object
+      newContent += '<div class="event">';
+      newContent += '<img src="' + responseObject.events[i].map + '" ';
+      newContent += 'height = "300" width = "300"  ';
+      newContent += 'alt="' + responseObject.events[i].location + '" />';
+      newContent += '<p><b>' + responseObject.events[i].location + '</b><br>';
+      newContent += responseObject.events[i].date + '</p>';
+      newContent += '</div>';
+    }
+
+    // Update the page with the new content
+    document.getElementById('content').innerHTML = newContent;
+
+  //}
+};
+
+xhr.open('GET', "https://barrycumbie.github.io/cis-376/scripts/dev10-data.json", true);        // Prepare the request
+xhr.send(null);                                 // Send the request
